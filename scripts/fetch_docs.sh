5b582daf42f1c20131ffbea90e1adae645be6a79
Lonimokio <Arttu.mutka@gmail.com>
2025-02-24 15:01:37 +0200

    refactor(docs): update fetch_docs scripts to focus on .md files and streamline Docusaurus build process

--- scripts/fetch_docs.sh
+++ scripts/fetch_docs.sh
@@ -12,29 +12,29 @@ clone_repo() {
     git clone --recurse-submodules "$REPO_URL" "$CLONE_PATH"
 }
 
-# Fetch docs from a repository
-fetch_docs_from_repo() {
+# Fetch .md files from a repository
+fetch_md_files_from_repo() {
     local repo_path=$1
     local output_path=$2
 
-    find "$repo_path" -type d -name 'docs' | while read -r docs_path; do
-        relative_path=$(realpath --relative-to="$repo_path" "$docs_path")
+    find "$repo_path" -type f -name '*.md' | while read -r md_file; do
+        relative_path=$(realpath --relative-to="$repo_path" "$md_file")
         dest_path="$output_path/$relative_path"
         mkdir -p "$(dirname "$dest_path")"
-        cp -r "$docs_path" "$dest_path"
-        echo "Copied docs from $docs_path to $dest_path"
+        cp "$md_file" "$dest_path"
+        echo "Copied $md_file to $dest_path"
     done
 }
 
-# Fetch docs from submodules
-fetch_docs_from_submodules() {
+# Fetch .md files from submodules
+fetch_md_files_from_submodules() {
     local repo_path=$1
     local output_path=$2
 
     git -C "$repo_path" submodule foreach --recursive '
         submodule_path=$toplevel/$sm_path
         submodule_name=$(basename "$submodule_path")
-        fetch_docs_from_repo "$submodule_path" "$output_path/$submodule_name"
+        fetch_md_files_from_repo "$submodule_path" "$output_path/$submodule_name"
     '
 }
 
@@ -44,11 +44,11 @@ main() {
     clone_repo
     echo "Repository cloned to $CLONE_PATH"
 
-    echo "Fetching docs from repository at $CLONE_PATH to $OUTPUT_PATH"
-    fetch_docs_from_repo "$CLONE_PATH" "$OUTPUT_PATH"
+    echo "Fetching .md files from repository at $CLONE_PATH to $OUTPUT_PATH"
+    fetch_md_files_from_repo "$CLONE_PATH" "$OUTPUT_PATH"
 
-    echo "Fetching docs from submodules in repository at $CLONE_PATH to $OUTPUT_PATH"
-    fetch_docs_from_submodules "$CLONE_PATH" "$OUTPUT_PATH"
+    echo "Fetching .md files from submodules in repository at $CLONE_PATH to $OUTPUT_PATH"
+    fetch_md_files_from_submodules "$CLONE_PATH" "$OUTPUT_PATH"
     echo "Fetching completed"
 }
 
